fcc02248b9e6b1da4411b5d48f56df7a533e378d
matveySV <ihtmatvienko@gmail.com>
2023-01-13 18:48:11 +0200

    empty project with added spring dependency and liquibase dependency

---
+++ src/test/java/com/example/nix_spring1/DemoNixSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nix_spring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoNixSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
